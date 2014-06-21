d5f5bd9db51fcfc4d7f51975ef590d48f2641908
Marton Tasnadi <marton.tasnadi@gmail.com>
2014-06-21 15:50:24 +0200

    js libs and seo

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html <?php language_attributies; ?> xmlns="http://www.w3.org/1999/xhtml">
+<html lang="hu-HU" xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <meta charset="utf-8">
     <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=Edge">-->
@@ -7,11 +7,26 @@
 
     <title><?php if (is_front_page()) { bloginfo('name'); } else { wp_title(''); } ?></title>
 
+    <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+    
+    <!-- JavaScript -->
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/vendor/respond.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/vendor/html5shiv.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/vendor/jquery.js"></script>
 
-    <script type="text/javascript" src="/js/jquery.jsj"></script>
+    <!-- wp_head(); -->
     <?php wp_head(); ?>
+
+    <!-- SEO -->
+    <meta name="author" content="Tasnádi Márton (kistasi)">
+    <meta name="description" content="Tasnádi Márton személyes és tech blogoldala.">
+    <meta name="keywords" content="énblog, lélek, drupal, wordpress, linux, opensuse, ubuntu, debian, fedora, windows, mac, osx, waldorf, thinkpad, tasi, kistasi, tasnádi márton, weboldal készítés, unix, unix-like, webmester, webfejlesztés, webfejlesztő">
+    <meta property="og:copyright" content="kistasiblog (c) 2010-2014">
+    <meta property="og:title" content="kistasiblog">
+    <meta property="og:type" content="blog">
+    <meta property="og:url" content="http://team4world.hu/">
   </head>
   <body <?php body_class(); ?>>
     <header>
