2e133a490041769320edc942da3acdd2c3b4adc6
Marton Tasnadi <marton.tasnadi@gmail.com>
2014-07-05 23:48:39 +0200

    footer links

--- footer.php
+++ footer.php
@@ -1,7 +1,15 @@
       </div>
     </main>
     <footer id="blog-footer">
-      <span>kistasiblog (c) 2010-2014</span>
+      <div id="blog-footer-links">
+        <ul>
+          <li><a target="_blank" href="https://facebook.com/tasi.kistasi">Facebook</a></li>
+          <li><a target="_blank" href="http://twitter.com/team4world">Twitter</a></li>
+          <li><a target="_blank" href="http://instagram.com/kistasi">Instagram</a></li>
+          <li><a target="_blank" href="http://github.com/kistasi">GitHub</a></li>
+        </ul>
+      </div>
+      <span id="blog-footer-copyright">kistasiblog (c) 2010-2014</span>
     </footer>
     <script type="text/javascript">
     	var _gaq = _gaq || [];
