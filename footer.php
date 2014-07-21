5deb8f6a6adef31c54dd8e28ad6d37bd2809a24e
Marton Tasnadi <marton.tasnadi@gmail.com>
2014-07-22 01:43:04 +0200

    simpler footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
       </div>
     </main>
     <footer id="blog-footer">
-      <div id="blog-footer-links">
+      <div id="social-links">
         <ul>
           <li><a target="_blank" href="https://facebook.com/tasi.kistasi">Facebook</a></li>
           <li><a target="_blank" href="http://twitter.com/team4world">Twitter</a></li>
@@ -9,7 +9,7 @@
           <li><a target="_blank" href="http://github.com/kistasi">GitHub</a></li>
         </ul>
       </div>
-      <span id="blog-footer-copyright">kistasiblog &copy; 2010-2014</span>
+      <span id="copyright">kistasiblog &copy; 2010-2014</span>
     </footer>
 <?php wp_footer(); ?>
   </body>
