009116295230650795f0cbb6f5c1e3ab231d4f08
Marton Tasnadi <marton.tasnadi@gmail.com>
2014-07-25 02:52:50 +0200

    prefix of the page id

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
-<div id="<?php the_ID(); ?>" <?php post_class(); ?>>
+<div id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="page-header">
 		<h3 class="page-title"><?php the_title(); ?></h3>
 	</header>
